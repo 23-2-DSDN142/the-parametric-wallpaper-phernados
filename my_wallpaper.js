dddffcf61fb2f548c345feb42cfe6cd34ceebf5a
phernados <g.e.evans0793@gmail.com>
2023-07-24 10:09:12 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,7 +4,7 @@ let rect_height = 20;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+   pWallpaper.output_mode(GLIDE_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -19,5 +19,16 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+ 
+  ellipse(100, 100, 60, 60); // draws a circle 45 pixels accross at location 100 pixels accross and 100 pixels down
+  ellipse(80, 80, 40, 40);//head 
+  ellipse(85, 80, 5, 5); // eye
+
+  line(70,85,50,100) //beek 
+
+  line(100, 130, 100, 150) //leg
+  line(90, 150, 100, 150)
+
+
+
 }
